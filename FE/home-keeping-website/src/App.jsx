724f54ae8267392c74c6acc34ab722f8af6ac720
TuanLam123456 <130367417+TuanLam123456@users.noreply.github.com>
2025-02-25 10:16:04 +0700

    Login by Google

--- FE/home-keeping-website/src/App.jsx
+++ FE/home-keeping-website/src/App.jsx
@@ -1,9 +1,16 @@
 import AppRoutes from './routes';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const GOOGLE_CLIENT_ID = "389719592750-1bnfd3k1g787t8r8tmvltrfokvm87ur2.apps.googleusercontent.com"; // Replace this with your actual Client ID
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <AppRoutes />
+    </GoogleOAuthProvider>
+  );
 }
 
 export default App;
