d91822ecd0f72b308f721a15f002e739745cd7bb
SnoweyMTT <nhule.251100@gmail.com>
2025-03-05 21:37:24 +0700

    Cập nhật package-lock.json, package.json, App.js, index.js và xóa file không cần thiết

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "./assets/styles/App.css";
+import "./i18n/i18n";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { UserRoleProvider } from "./components/UserRoleProvider";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import HousekeeperDashboard from "./pages/HousekeeperDashboard";
+import FamilyDashboard from "./pages/FamilyDashboard";
+
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserRoleProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/housekeeper/home" element={<HousekeeperDashboard />} />
+            <Route path="/family/home" element={<FamilyDashboard />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </UserRoleProvider>
   );
 }
 
