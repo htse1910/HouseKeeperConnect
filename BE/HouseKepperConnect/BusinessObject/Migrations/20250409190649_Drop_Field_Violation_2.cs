99f94c7e7e715cde2f6834a8782e2de61cc67497
Moteyabai <134784925+Moteyabai@users.noreply.github.com>
2025-04-10 02:11:50 +0700

    Fix Violation Migration

---
+++ BE/HouseKepperConnect/BusinessObject/Migrations/20250409190649_Drop_Field_Violation_2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Drop_Field_Violation_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+            name: "FK_Housekeeper_Violation_ViolationID",
+            table: "Housekeeper");
+
+            migrationBuilder.DropIndex(
+            name: "IX_Housekeeper_ViolationID",
+            table: "Housekeeper");
+
+            migrationBuilder.DropColumn(
+                name: "ViolationID",
+                table: "Housekeeper");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
