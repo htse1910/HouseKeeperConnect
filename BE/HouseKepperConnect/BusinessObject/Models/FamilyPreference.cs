f93da5f11689cca878021b276a52ec143ef6a897
hoangduong1910 <hoangtrieuduong1910@gmail.com>
2025-05-15 22:23:08 +0700

    add migration FamilyPreference

---
+++ BE/HouseKepperConnect/BusinessObject/Models/FamilyPreference.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BusinessObject.Models
+{
+    public class FamilyPreference
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FamilyPreferenceID { get; set; }
+        [ForeignKey("Family")]
+        public int FamilyID { get; set; }
+        public int GenderPreference {  get; set; }
+        public int LanguagePreference {  get; set; }
+        public int LocationPreference { get; set; }
+        public virtual Family Family { get; set; }
+    }
+}
